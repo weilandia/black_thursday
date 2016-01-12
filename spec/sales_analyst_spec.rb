d98396f629aaff2ebb4f01e6424fc074388d8333
Nick Weiland <nickweiland@gmail.com>
2016-01-12 12:01:14 -0700

    Add frames for SalesAnalyst class

---
+++ spec/sales_analyst_spec.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/sales_analyst'
+
+class SalesAnalystTest < Minitest::Test
+
+
+end
