9757d23f85dd8f3140b48b8c027d56a28710a098
Nick Weiland <nickweiland@gmail.com>
2016-01-12 11:38:31 -0700

    Add integrated tests for SalesEngine

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -21,32 +21,32 @@ class SalesEngineTest < Minitest::Test
     assert_equal ItemRepository, @sales_engine.items.class
   end
 
-  def test_data_files_hash_method_defaults_correct_files
+  def test_sales_engine_data_files_hash_method_defaults_correct_files
     assert_equal @sales_engine.data_files_hash[:merchants], "./data/merchants.csv"
 
     assert_equal @sales_engine.data_files_hash[:items], "./data/items.csv"
   end
 
   # Integrated SalesEngine tests
-  def test_sales_engine_obj_has_access_to_merhant_id
+  def test_sales_engine_object_has_access_to_merhant_id
     merchant_id = @sales_engine.merchants.find_by_name("MiniatureBikez").id
 
     assert_equal "12334113", merchant_id
   end
 
-  def test_sales_engine_obj_has_access_to_merhant_name
+  def test_sales_engine_object_has_access_to_merhant_name
     merchant_name = @sales_engine.merchants.find_by_id(12334113).name
 
     assert_equal "MiniatureBikez", merchant_name
   end
 
-  def test_sales_engine_obj_has_access_to_item_id
+  def test_sales_engine_object_has_access_to_item_id
     item_id = @sales_engine.items.find_by_name("Very Magnifique").id
 
     assert_equal "263404435", item_id
   end
 
-  def test_sales_engine_obj_has_access_item_id
+  def test_sales_engine_object_has_access_item_name
     item_name = @sales_engine.items.find_by_id(263404435).name
 
     assert_equal "Very Magnifique", item_name
