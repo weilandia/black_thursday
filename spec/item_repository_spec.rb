484b1b2526edb44d97e6f1f4781daa66796e91cb
Nick Weiland <nickweiland@gmail.com>
2016-01-19 13:05:50 -0700

    Decouple item repo tests

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -3,10 +3,6 @@ require_relative '../lib/item_repository'
 require_relative '../lib/data_parser'
 
 class ItemRepositoryTest < Minitest::Test
-  def setup
-    @item_repo = test_helper_item_repo
-  end
-
   def test_item_repo_queries_all_items
     i = ItemRepository.new
 
@@ -23,74 +19,132 @@ class ItemRepositoryTest < Minitest::Test
   end
 
   def test_item_repo_finds_item_by_id
-    item = @item_repo.find_by_id(10)
+    i = ItemRepository.new
 
-    assert_equal Item, item.class
+    item_one = Item.new({id: 1})
+    item_two = Item.new({id: 2})
 
-    assert_equal 10, item.id
+    i.all_items << item_one
+    i.all_items << item_two
+
+    assert_equal item_one, i.find_by_id(1)
+    assert_equal item_two, i.find_by_id(2)
   end
 
   def test_item_repo_finds_item_by_name
-    item = @item_repo.find_by_name("Magnifique")
+    i = ItemRepository.new
 
-    assert_equal Item, item.class
+    item_one = Item.new({name: "Book"})
+    item_two = Item.new({name: "eBook"})
 
-    assert_equal "Magnifique", item.name
+    i.all_items << item_one
+    i.all_items << item_two
+
+    assert_equal item_one, i.find_by_name("Book")
+    assert_equal item_one, i.find_by_name("bOOK")
+    assert_equal item_two, i.find_by_name("eBook")
   end
 
   def test_item_repo_returns_message_when_search_not_found
-    item_one = @item_repo.find_by_name("Turing")
+    i = ItemRepository.new
 
-    item_two = @item_repo.find_by_id("hello")
+    item_one = Item.new({name: "Book"})
+    item_two = Item.new({name: "eBook"})
 
-    assert_equal nil, item_one
+    i.all_items << item_one
+    i.all_items << item_two
 
-    assert_equal nil, item_two
+    assert_equal nil, i.find_by_name("Newspaper")
   end
 
   def test_item_repo_finds_array_of_items_matching_name_search_fragment
-    search_array_names = @item_repo.find_all_by_name("Mag").map do |item|
-      item.name
-    end
-    assert_equal ["Magnifique", "Very Magnifique"], search_array_names
+    i = ItemRepository.new
+
+    item_one = Item.new({name: "Ven"})
+    item_two = Item.new({name: "Venmo"})
+    item_three = Item.new({name: "Myven"})
+    item_four = Item.new({name: "Myv en"})
+
+    i.all_items << item_one
+    i.all_items << item_two
+    i.all_items << item_three
+    i.all_items << item_four
+
+    assert_equal [item_one, item_two, item_three], i.find_all_by_name("Ve")
   end
 
   def test_item_repo_name_fragment_search_is_case_insensitive
-    search_array_names = @item_repo.find_all_by_name("mag").map do |item|
-      item.name
-    end
-    assert_equal ["Magnifique", "Very Magnifique"], search_array_names
+    i = ItemRepository.new
+
+    item_one = Item.new({name: "Ven"})
+    item_two = Item.new({name: "Venmo"})
+    item_three = Item.new({name: "Myven"})
+    item_four = Item.new({name: "Myv en"})
+
+    i.all_items << item_one
+    i.all_items << item_two
+    i.all_items << item_three
+    i.all_items << item_four
+
+    assert_equal [item_one, item_two, item_three], i.find_all_by_name("Ve")
   end
 
   def test_item_repo_finds_array_of_items_matching_description_search_fragment
-    search_array_description = @item_repo.find_all_with_description("decorative lighted").map do |item|
-      item.name
-    end
-    assert_equal ["Shimmering Peacock"], search_array_description
+    i = ItemRepository.new
+
+    item_one = Item.new({name: "Book", description: "You read it."})
+    item_two = Item.new({name: "eBook", description: "A book, without paper. Read it!"})
+
+    i.all_items << item_one
+    i.all_items << item_two
+
+    assert_equal [item_one, item_two], i.find_all_with_description("read")
+    assert_equal [item_one], i.find_all_with_description("yoU")
   end
 
   def test_item_repo_finds_array_of_items_mathcing_price
-    search_array_price = @item_repo.find_all_by_price(80000).map do |item|
-      item.name
-    end
-    assert_equal ["Very Magnifique", "TestItem28", "TestItem29"], search_array_price
+    i = ItemRepository.new
+
+    item_one = Item.new({unit_price: 8000})
+    item_two = Item.new({unit_price: 7000})
+    item_three = Item.new({unit_price: 8000})
+
+    i.all_items << item_one
+    i.all_items << item_two
+    i.all_items << item_three
+
+    assert_equal [item_one, item_three], i.find_all_by_price(8000)
+    assert_equal [item_two], i.find_all_by_price(7000)
   end
 
   def test_item_repo_finds_array_of_all_items_in_price_range
-    search_array_price = @item_repo.find_all_by_price_in_range(75000..85000).map do |item|
-      item.name
-    end
+    i = ItemRepository.new
 
-    result = ["Very Magnifique", "TestItem28", "TestItem29"]
+    item_one = Item.new({unit_price: 8000})
+    item_two = Item.new({unit_price: 7000})
+    item_three = Item.new({unit_price: 8000})
 
-    assert_equal result, search_array_price
+    i.all_items << item_one
+    i.all_items << item_two
+    i.all_items << item_three
+
+    assert_equal [item_one, item_two, item_three], i.find_all_by_price_in_range(6500..8500)
+    assert_equal [item_two], i.find_all_by_price_in_range(6500..7500)
   end
 
   def test_item_repo_finds_array_of_all_items_given_merchant_id
-    search_array = @item_repo.find_all_by_merchant_id(3).map do |item|
-      item.name
-    end
-    assert_equal ["TestItem12", "TestItem35"], search_array
+    i = ItemRepository.new
+
+    item_one = Item.new({id: 1, merchant_id: 1})
+    item_two = Item.new({id: 2,  merchant_id: 1})
+    item_three = Item.new({id: 3,  merchant_id: 2})
+
+    i.all_items << item_one
+    i.all_items << item_two
+    i.all_items << item_three
+
+    assert_equal [item_one, item_two], i.find_all_by_merchant_id(1)
+    assert_equal [item_three], i.find_all_by_merchant_id(2)
   end
 
 end
