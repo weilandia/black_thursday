026db49fc5f22647ce7f4b6d1f2b1a74a08e710c
Nick Weiland <nickweiland@gmail.com>
2016-01-19 21:09:14 -0700

    Add merchant_anlysis refactor

--- lib/merchant_analysis.rb
+++ lib/merchant_analysis.rb
@@ -8,7 +8,7 @@ module MerchantAnalysis
   end
 
   def item_count_per_merchant
-   engine.merchants.all.map { |m| m.items.count }
+    engine.merchants.all.map { |m| m.items.count }
   end
 
   def average_items_per_merchant
@@ -22,13 +22,13 @@ module MerchantAnalysis
   def merchants_with_low_item_count
     sd = average_items_per_merchant_standard_deviation
     avg = average_items_per_merchant
-    all_merchants.map { |m| m if m.items.count <=  avg - sd }.compact
+    all_merchants.map { |m| m if m.items.count <= avg - sd }.compact
   end
 
   def merchants_with_high_item_count
     sd = average_items_per_merchant_standard_deviation
     avg = average_items_per_merchant
-    all_merchants.map { |m| m if m.items.count >=  avg + sd }.compact
+    all_merchants.map { |m| m if m.items.count >= avg + sd }.compact
   end
 
   def average_average_price_per_merchant
@@ -38,7 +38,7 @@ module MerchantAnalysis
 
   def average_item_price_for_merchant(merchant_id)
     merchant = engine.merchants.find_by_id(merchant_id)
-    itm_prices = merchant.items.map { |i| i.unit_price }.compact
+    itm_prices = merchant.items.map(&:unit_price).compact
     return 0.0 if itm_prices.empty?
     average = (itm_prices.inject(0, :+) / merchant.items.count)
     (average / 100).round(2)
@@ -47,21 +47,21 @@ module MerchantAnalysis
   def top_merchants_by_invoice_count
     sd = average_invoices_per_merchant_standard_deviation
     avg = average_invoices_per_merchant
-    all_merchants.map { |m| m if m.invoices.count >=  avg + (2 * sd) }.compact
+    all_merchants.map { |m| m if m.invoices.count >= avg + (2 * sd) }.compact
   end
 
   def bottom_merchants_by_invoice_count
     sd = average_invoices_per_merchant_standard_deviation
     avg = average_invoices_per_merchant
-    all_merchants.map { |m| m if m.invoices.count <=  avg - (2 * sd) }.compact
+    all_merchants.map { |m| m if m.invoices.count <= avg - (2 * sd) }.compact
   end
 
   def merchants_with_revenue
-    all_merchants.reject { |m| m.revenue == nil || m.revenue == 0 }
+    all_merchants.reject { |m| m.revenue.nil? || m.revenue == 0 }
   end
 
   def merchants_ranked_by_revenue
-    merchants_with_revenue.sort_by { |m| m.revenue }.reverse
+    merchants_with_revenue.sort_by(&:revenue).reverse
   end
 
   def top_revenue_earners(x = 20)
@@ -90,22 +90,22 @@ module MerchantAnalysis
 
   def revenue_by_merchant(merchant_id)
     merchant = engine.merchants.find_by_id(merchant_id)
-    invoices = merchant.invoices.select { |i| i.is_paid_in_full? }
-    invoices.map { |i| i.total }.inject(:+)
+    invoices = merchant.invoices.select(&:is_paid_in_full?)
+    invoices.map(&:total).inject(:+)
   end
 
   def most_sold_item_for_merchant(merchant_id)
     merch = engine.merchants.find_by_id(merchant_id)
     invoices = merch.invoices.select { |i| i.is_paid_in_full? }
-    inv_itms = invoices.map { |i| i.invoice_items }.flatten
-    most_sold = inv_itms.sort_by { |i| i.quantity }.last
+    inv_itms = invoices.map(&:invoice_items).flatten
+    most_sold = inv_itms.sort_by(&:quantity).last
     return nil if most_sold.nil?
     engine.items.find_by_id(most_sold.item_id)
   end
 
   def best_item_for_merchant(merchant_id)
     merch = engine.merchants.find_by_id(merchant_id)
-    invoices = merch.invoices.select { |i| i.is_paid_in_full? }
+    invoices = merch.invoices.select(&:is_paid_in_full?)
     inv_itms = invoices.map { |i| i.invoice_items }.flatten
     best_item = inv_itms.sort_by { |i| i.unit_price * i.quantity }.last
     return nil if best_item.nil?
